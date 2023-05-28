67c43eaaec552efc8cc9f60ab2db68e7e18d9275
Matheus Duarte <matheusdcarvalho007@gmail.com>
2023-05-28 13:11:28 -0300

    [feat] add logger module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './common/database/database.module';
+import { LoggerModule } from './common/providers/logging/logger.module';
 import { PaymentsModule } from './modules/payments/payments.module';
 
 @Module({
   imports: [
+    LoggerModule,
     DatabaseModule,
     ConfigModule.forRoot({
       isGlobal: true,
