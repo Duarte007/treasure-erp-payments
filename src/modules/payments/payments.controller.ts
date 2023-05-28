d5180abe9859d7c6c30fc74a03c6d281678bf18a
Matheus Duarte <matheusdcarvalho007@gmail.com>
2023-05-28 12:57:08 -0300

    [fix] add pubsub decorator to create payment from order event method

--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { TokenAuthQueryParam } from '../../common/dtos/token-auth-qs.dto';
 import { PubsubPostBody } from '../../common/pubsub/interfaces/pubsub-post-body';
+import { PubSub } from '../../common/pubsub/pubsub.decorator';
 import { CreatePaymentDto, OrderEventDTO } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
 import { PaymentsService } from './services/payments.service';
@@ -24,6 +25,7 @@ export class PaymentsController {
   }
 
   @Post('order-event')
+  @PubSub()
   createFromOrderEvent(
     @Body() orderEvent: PubsubPostBody,
     @Query() params: TokenAuthQueryParam,
